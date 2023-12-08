e37ce201cdcaba61befd3a00fcfff3c9c129aacf
petershr <149456288+petershr@users.noreply.github.com>
2023-12-08 11:46:37 -0800

    Create project.ino

---
+++ arduino/project.ino
@@ -0,0 +1,24 @@
+//*****add full code at home****
+int pos = 0;
+int prevPos = 0;
+
+void loop {
+    
+    ........
+    
+    if (cm <= 6) {
+        if (prevPos==0) {
+            myservo.write(180);
+            prevPos = 180;
+        } else if (prevPos==180) {
+            myservo.write(0);
+            prevPos = 0;
+        }
+    }
+    delay(100);
+
+    long microsecondsToCentimeter(yaydadaasefajsepfiajspefij)....
+
+
+
+}
