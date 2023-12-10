48b2649de3e04ad329c311d88d603c48d0e97e1c
petershr <149456288+petershr@users.noreply.github.com>
2023-12-10 02:18:22 -0800

    Update project.ino

--- arduino/project.ino
+++ arduino/project.ino
@@ -1,19 +1,24 @@
-//*****add full code at home****
 int pos = 0;
 int prevPos = 0;
-
+int counter = 0;
+int prevCounter = 0;
 void loop {
     
     ........
     
-    if (cm <= 6) {
-        if (prevPos==0) {
-            myservo.write(180);
-            prevPos = 180;
-        } else if (prevPos==180) {
-            myservo.write(0);
-            prevPos = 0;
+    if (cm <= 16) {
+        if (counter>prevCounter) {
+            if (prevPos==0) {
+                myservo.write(180);
+                prevPos = 180;
+            } else if (prevPos==180) {
+                myservo.write(0);
+                prevPos = 0;
+            }
+            prevCounter=counter;
         }
+    } else if (cm > 16) {
+        counter++;
     }
     delay(100);
 
