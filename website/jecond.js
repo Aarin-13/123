4c7a087a9acd7e562c581791e99160573d1624e6
petershr <peter67shr@gmail.com>
2023-12-13 22:48:11 -0800

    Rearranged main website

---
+++ website/jecond.js
@@ -0,0 +1,30 @@
+var box1 = document.querySelector('.numberOne');
+var image1 = document.querySelector('.bottomOne');
+    
+    box1.addEventListener('mouseover', function() {
+        image1.style.opacity = '100%';
+    });
+    
+    box1.addEventListener('mouseout', function() {
+        image1.style.opacity = '0%';
+    });
+var box2 = document.querySelector('.numberTwo');
+var image2 = document.querySelector('.bottomTwo');
+    
+    box2.addEventListener('mouseover', function() {
+        image2.style.opacity = '100%';
+    });
+    
+    box2.addEventListener('mouseout', function() {
+        image2.style.opacity = '0%';
+    });
+var box3 = document.querySelector('.numberThree');
+var image3 = document.querySelector('.bottomThree');
+    
+    box3.addEventListener('mouseover', function() {
+        image3.style.opacity = '100%';
+    });
+    
+    box3.addEventListener('mouseout', function() {
+        image3.style.opacity = '0%';
+    });
