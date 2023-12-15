65ba7faa2c52801adc16def39aa1be4a88f5fa67
petershr <peter67shr@gmail.com>
2023-12-15 00:27:48 -0800

    finished arduino project/website

--- arduino/project.ino
+++ arduino/project.ino
@@ -7,11 +7,7 @@ int prevPos = 0;
 int counter = 0;
 int prevCounter = 0;
 const int pingPin = 7;
-//NewPing ultra_left(TRIGGERL, ECHOL, MAX_DISTANCE);
-//NewPing ultra_bottom_left(TRIGGERBL, ECHOBL, MAX_DISTANCE);
-//NewPing ultra_top(TRIGGERT, ECHOT, MAX_DISTANCE);
-//NewPing ultra_bottom_right(TRIGGERBR, ECHOBR, MAX_DISTANCE);
-//NewPing ultra_right(TRIGGERR, ECHOR, MAX_DISTANCE); // initializes the pings from the ultrasonic sensors
+
 
 void setup() {
   myservo.attach(9); // attaches the pins of the servos to the servo object
@@ -26,29 +22,11 @@ void setup() {
 
 
 
-void prepGrab() { // prepare to grab the payload
-  myservo.write(180); // the jaw opens
-}
 
-void grab() {
-  myservo.write(0);
-}
 
 
 void loop() {
- // unsigned int distanceL = ultra_left.ping_cm();
-  //unsigned int distanceBL = ultra_bottom_left.ping_cm();
-  //unsigned int distanceT = ultra_top.ping_cm();
- // unsigned int distanceBR = ultra_bottom_right.ping_cm();
- // unsigned int distanceR = ultra_right.ping_cm(); // grabs the distance from the ultrasound
- 
- // while (counter>0) {
- //   prepGrab();
- //   delay(1000);
- //   grab();
- //   delay(1000);
- //   counter--;
- // }
+
   long duration, cm;
   pinMode(pingPin, OUTPUT);
   digitalWrite(pingPin, LOW);
@@ -66,9 +44,9 @@ void loop() {
   Serial.println();
 
   
-  delay(500);
+  delay(75);
   
-  if (cm <= 16) {
+  if (cm <= 50) {
         if (counter>prevCounter) {
             if (prevPos==0) {
                 myservo.write(180);
@@ -79,27 +57,11 @@ void loop() {
             }
             prevCounter=counter;
         }
-    } else if (cm > 16) {
+    } else if (cm > 50) {
         counter++;
     }
-        /*if (prevPos==0) {
-            myservo.write(180);
-            prevPos = 180;
-        } else if (prevPos==180) {
-            myservo.write(0);
-            prevPos = 0;
-        }*/
-        /***if (counter%2==0) {
-          myservo.write(180);
-          counter+=0.5;
-        } else if (counter%2==1) {
-          myservo.write(0);
-          counter+=0.5;
-        }
-  } else {
-    counter+=0.5;
-  }***/
-    delay(500);
+            
+    delay(75);
 }// plan use prep grab and say if the degree is 0 change to 180 if degree is 180 change to 0 and then find a way to make the if command detect a chance in distance and FLIP
 // so when i move my hand in front of it and leave - it rotates only one time - just like walking through a door, the sensor detects a close reading then a far reading, but it should flick the lever only once
 
