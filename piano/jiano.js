b13da0c407655d41a05ae4f16caa5646b69664d3
petershr <peter67shr@gmail.com>
2023-12-14 23:00:48 -0800

    added piano sounds

--- piano/jiano.js
+++ piano/jiano.js
@@ -11,13 +11,212 @@ tl.to('.key', {
   
 })
 
-const audio = new Audio("notec.mp3");
+const audioc = new Audio("c.mp4");
 const buttons = document.querySelectorAll(".C");
 
 buttons.forEach(button => {
   button.addEventListener("click", () => {
-    audio.play();
+    audioc.play();
+  });
+});
+const audiocs = new Audio("cs.mp4");
+const buttonss = document.querySelectorAll(".Cs");
+
+buttonss.forEach(button => {
+  button.addEventListener("click", () => {
+    audiocs.play();
+  });
+});
+const audiod = new Audio("d.mp4");
+const buttonsss = document.querySelectorAll(".D");
+
+buttonsss.forEach(button => {
+  button.addEventListener("click", () => {
+    audiod.play();
+  });
+});
+const audiods = new Audio("ds.mp4");
+const buttonssss = document.querySelectorAll(".Ds");
+
+buttonssss.forEach(button => {
+  button.addEventListener("click", () => {
+    audiods.play();
+  });
+});
+const audioe = new Audio("e.mp4");
+const buttonsssss = document.querySelectorAll(".E");
+
+buttonsssss.forEach(button => {
+  button.addEventListener("click", () => {
+    audioe.play();
+  });
+});
+const audiof = new Audio("f.mp4");
+const buttonssssss = document.querySelectorAll(".F");
+
+buttonssssss.forEach(button => {
+  button.addEventListener("click", () => {
+    audiof.play();
+  });
+});
+const audiofs = new Audio("fs.mp4");
+const buttonsssssss = document.querySelectorAll(".Fs");
+
+buttonsssssss.forEach(button => {
+  button.addEventListener("click", () => {
+    audiofs.play();
+  });
+});
+const audiog = new Audio("g.mp4");
+const buttonssssssss = document.querySelectorAll(".G");
+
+buttonssssssss.forEach(button => {
+  button.addEventListener("click", () => {
+    audiog.play();
+  });
+});
+const audiogs = new Audio("gs.mp4");
+const buttonsssssssss = document.querySelectorAll(".Gs");
+
+buttonsssssssss.forEach(button => {
+  button.addEventListener("click", () => {
+    audiogs.play();
+  });
+});
+const audioa = new Audio("a.mp4");
+const buttonssssssssss = document.querySelectorAll(".A");
+
+buttonssssssssss.forEach(button => {
+  button.addEventListener("click", () => {
+    audioa.play();
+  });
+});
+const audioas = new Audio("as.mp4");
+const buttonsssssssssss = document.querySelectorAll(".As");
+
+buttonsssssssssss.forEach(button => {
+  button.addEventListener("click", () => {
+    audioas.play();
+  });
+});
+const audiob = new Audio("b.mp4");
+const buttonssssssssssss = document.querySelectorAll(".B");
+
+buttonssssssssssss.forEach(button => {
+  button.addEventListener("click", () => {
+    audiob.play();
+  });
+});
+const audiomaj = new Audio("c.mp4");
+const abutton = document.querySelectorAll(".maj");
+
+abutton.forEach(button => {
+  button.addEventListener("click", () => {
+    audiomaj.play();
+  });
+});
+const audiomin = new Audio("minor.mp4");
+const abuttons = document.querySelectorAll(".min");
+
+abuttons.forEach(button => {
+  button.addEventListener("click", () => {
+    audiomin.play();
+  });
+});
+const audioarp = new Audio("arp.mp4");
+const abuttonss = document.querySelectorAll(".arp");
+
+abuttonss.forEach(button => {
+  button.addEventListener("click", () => {
+    audioarp.play();
+  });
+});
+const audioaug = new Audio("aug.mp4");
+const abut = document.querySelectorAll(".aug");
+
+abut.forEach(button => {
+  button.addEventListener("click", () => {
+    audioaug.play();
   });
 });
 
-//https://soundboardguy.com/sounds/c-note-mp3/
+
+
+
+
+const audiodim = new Audio("dim.mp4");
+const abuts = document.querySelectorAll(".dim");
+
+abuts.forEach(button => {
+  button.addEventListener("click", () => {
+    audiodim.play();
+  });
+});
+
+
+
+
+const audiomaj7 = new Audio("maj7.mp4");
+const abutss = document.querySelectorAll(".maj7");
+
+abutss.forEach(button => {
+  button.addEventListener("click", () => {
+    audiomaj7.play();
+  });
+});
+const audiodom = new Audio("dom.mp4");
+const abutsss = document.querySelectorAll(".dom");
+
+abutsss.forEach(button => {
+  button.addEventListener("click", () => {
+    audiodom.play();
+  });
+});
+const audioaug7 = new Audio("aug7.mp4");
+const abutssss = document.querySelectorAll(".aug7");
+
+abutssss.forEach(button => {
+  button.addEventListener("click", () => {
+    audioaug7.play();
+  });
+});
+const audioaugmaj = new Audio("augmaj.mp4");
+const abutsssss = document.querySelectorAll(".augmaj");
+
+abutsssss.forEach(button => {
+  button.addEventListener("click", () => {
+    audioaugmaj.play();
+  });
+});
+const audiomin7 = new Audio("min7.mp4");
+const abutssssss = document.querySelectorAll(".min7");
+
+abutssssss.forEach(button => {
+  button.addEventListener("click", () => {
+    audiomin7.play();
+  });
+});
+const audiominmaj = new Audio("minmaj.mp4");
+const abutsssssss = document.querySelectorAll(".minmaj");
+
+abutsssssss.forEach(button => {
+  button.addEventListener("click", () => {
+    audiominmaj.play();
+  });
+});
+const audiohalf = new Audio("half.mp4");
+const abutssssssss = document.querySelectorAll(".half");
+
+abutssssssss.forEach(button => {
+  button.addEventListener("click", () => {
+    audiohalf.play();
+  });
+});
+const audiodim7 = new Audio("dim7.mp4");
+const abutsssssssss = document.querySelectorAll(".dim7");
+
+abutsssssssss.forEach(button => {
+  button.addEventListener("click", () => {
+    audiodim7.play();
+  });
+});
